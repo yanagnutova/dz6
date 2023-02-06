512de354597f76744a115eac8d6b203adcb16443
yanagnutova <yanagnutova@icloud.com>
2023-02-06 20:02:41 +0400

    first commit

---
+++ sem6/task1/Program.cs
@@ -0,0 +1,41 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int InputIntArray(string message)
+{
+    Console.Write(message + "  ");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+int[] CreateNumbersArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
+int PositiveNumbers (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count ++;
+        }
+    }
+    Console.WriteLine($"Количество положительных элементов {count}");
+    return count;
+}
+
+int elements = InputIntArray("Введите количество элементов в массиве");
+int[] array = new int[elements];
+int[] numbersArray = CreateNumbersArray(array);
+int pos = PositiveNumbers(array);
+
---
+++ sem6/task2/Program.cs
@@ -0,0 +1,45 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int InputCoef(string message)
+{
+    Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double FindX(double b1, double b2, double k1, double k2)
+{
+    double x = ((b2 - b1) / (k1 - k2));
+    return x;
+}
+
+double FindY(double k1, double b1, double k2, double b2)
+{
+    double x = FindX(b1, b2, k1, k2);
+    double y = (k1 * x + b1);
+    return y;
+}
+
+bool Validate(double k1, double k2)
+{
+    if (k1 == k2)
+    {
+        System.Console.WriteLine("Прямые параллельны или совпадают");
+        return false;
+    }
+    return true;
+}
+
+double b1 = InputCoef($"b1 = ");
+double b2 = InputCoef($"b2 = ");
+double k1 = InputCoef($"k1 = ");
+double k2 = InputCoef($"k2 = ");
+
+if (Validate(k1, k2))
+{
+    double resultX = FindX(b1, b2, k1, k2);
+    double resultY = FindY(k1, b1, k2, b2);
+    Console.WriteLine($"Точка пересечения двух прямых {resultX}, {resultY} ");
+}
